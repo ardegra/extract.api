69e8b1f214c863425acb7f77446ad540baefbd43
Frans Huang <frans@zannete.com>
2018-02-01 16:22:02 +0000

    bug fixes

--- lib/listener/ExtractCategoryLastPageURL.py
+++ lib/listener/ExtractCategoryLastPageURL.py
@@ -1,5 +1,6 @@
 """ ExtractLExtractCategoryLastPageURLastPageURL """
 import falcon
+import weblib
 
 from grab import Grab
 
@@ -16,16 +17,18 @@ class ExtractCategoryLastPageURL:
     grab = Grab()
     page = grab.go(url)
     
-    last_page_item = page.select(xpath["category"]["lastPage"])
-    last_page_url  = last_page_item.attr("href")
-    
-    if not "http://" in last_page_url:
-      if last_page_url[0] != "/":
-        last_page_url = "/{}".format(last_page_url)
-    
-    print("[ExtractCategoryLastPageURL] last_page_url: {}".format(last_page_url))
-    last_page_url  = grab.make_url_absolute(last_page_item.attr("href"), resolve_base=True)
-    result         = {"lastPageUrl": last_page_url}
-    
+    try:
+      last_page_item = page.select(xpath["category"]["lastPage"])
+      last_page_url  = last_page_item.attr("href")
+      
+      if not "http://" in last_page_url:
+        if last_page_url[0] != "/":
+          last_page_url = "/{}".format(last_page_url)
+      
+      print("[ExtractCategoryLastPageURL] last_page_url: {}".format(last_page_url))
+      last_page_url  = grab.make_url_absolute(last_page_item.attr("href"), resolve_base=True)
+      result         = {"lastPageUrl": last_page_url}
+    except weblib.error.DataNotFound as err:
+      result = {"lastPageUrl": None, "error": {"message": str(err), "code": 404}}
     res.context["result"] = result
     res.status            = falcon.HTTP_200
