348e8958500066f9c0805dad51c98775b9a0d536
Frans Huang <frans@zannete.com>
2018-02-01 16:18:26 +0000

    bug fixes

--- lib/listener/ExtractCategoryLastPageURL.py
+++ lib/listener/ExtractCategoryLastPageURL.py
@@ -19,12 +19,12 @@ class ExtractCategoryLastPageURL:
     last_page_item = page.select(xpath["category"]["lastPage"])
     last_page_url  = last_page_item.attr("href")
     
-    if "http://" not in last_page_url:
+    if not "http://" in last_page_url:
       if last_page_url[0] != "/":
         last_page_url = "/{}".format(last_page_url)
     
     print("[ExtractCategoryLastPageURL] last_page_url: {}".format(last_page_url))
-    last_page_url  = grab.make_url_absolute(last_page_item.attr("href"))
+    last_page_url  = grab.make_url_absolute(last_page_item.attr("href"), resolve_base=True)
     result         = {"lastPageUrl": last_page_url}
     
     res.context["result"] = result
