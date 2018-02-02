ffd4d5998dbc8e149fdd5c04f1e57f99709fbe21
Frans Huang <frans@zannete.com>
2018-02-02 09:25:18 +0000

    bug fixes

--- lib/listener/ExtractThreadURL.py
+++ lib/listener/ExtractThreadURL.py
@@ -18,6 +18,8 @@ class ExtractThreadURL:
     result           = {"threadList": []}
     thread_url_items = page.select(xpath["thread"]["url"])
     for thread_url in thread_url_items:
-      result["threadList"].append(grab.make_url_absolute(thread_url.attr("href")))
+      thread_url = thread_url.attr("href")
+      thread_url = grab.make_url_absolute(thread_url, resolve_base=True)
+      result["threadList"].append(thread_url)
     res.context["result"] = result
     res.status            = falcon.HTTP_200
