9e92eb431cd734604fc7fe86abbea1df846a8f72
Frans Huang <frans@zannete.com>
2018-02-03 04:08:03 +0000

    bug fixes

--- lib/parser/IndonesiaDateParser.py
+++ lib/parser/IndonesiaDateParser.py
@@ -3,27 +3,27 @@ class IndonesiaDateParser:
   @staticmethod
   def parse_month_to_number(month):
     month = month.lower()
-    if month == "januari":
+    if month == "januari" or month == "january":
       return "01"
-    elif month == "februari" or month == "febuari":
+    elif month == "februari" or month == "febuari" or month == "february":
       return "02"
-    elif month == "maret":
+    elif month == "maret" or month == "march":
       return "03"
     elif month == "april":
       return "04"
-    elif month == "mei":
+    elif month == "mei" or month == "may":
       return "05"
-    elif month == "juni":
+    elif month == "juni" or month == "june":
       return "06"
-    elif month == "juli":
+    elif month == "juli" or month == "july":
       return "07"
-    elif month == "agustus":
+    elif month == "agustus" or month == "august":
       return "08"
     elif month == "september":
       return "09"
-    elif month == "oktober":
+    elif month == "oktober" or month == "october":
       return "10"
     elif month == "nopember" or month == "november":
       return "11"
-    elif month == "desember":
+    elif month == "desember" or month == "december":
       return "12"
